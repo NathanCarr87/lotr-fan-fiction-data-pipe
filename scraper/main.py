9a7d171f3bbf090ae070bbd286b47cde73299cbb
Nathan Carr <carr.excel@gmail.com>
2023-11-13 16:08:14 -0700

    stuff

---
+++ scraper/main.py
@@ -0,0 +1,18 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get("https://www.fanfiction.net/book/Lord-of-the-Rings/'")
+print(driver.page_source)
+# assert "Python" in driver.title
+# elem = driver.find_element(By.NAME, "q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+driver.close()
+
